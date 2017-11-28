ea56a8f033adc75957ae534cd16a90e7d8305a78
Dwika Setya <dwikasm@gmail.com>
2017-11-28 19:11:34 +0700

    Using butterknife mainActivity

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/MainActivity.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/MainActivity.java
@@ -1,8 +1,14 @@
 package com.study.dwika.kplchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.study.dwika.kplchat.login.LoginActivity;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = (Button) findViewById(R.id.btnLogin);
-        loginButton.setOnClickListener(ocl);
     }
 
-    @OnClick
+    @OnClick(R.id.btnLogin)
+    void onLoginClick(View v){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        MainActivity.this.startActivity(intent);
+        finish();
+    }
+
 }
