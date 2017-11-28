b3ad0a57e6c7b00ae841aa5c75ca304a98621ae3
Dwika Setya <dwikasm@gmail.com>
2017-11-28 19:34:46 +0700

    LoginActivity update

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/MainActivity.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/MainActivity.java
@@ -4,22 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.study.dwika.kplchat.login.LoginActivity;
 
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    @OnClick(R.id.btnLogin)
+    @OnClick(R.id.btn_login)
     void onLoginClick(View v){
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         MainActivity.this.startActivity(intent);
--- KPLChat/app/src/main/java/com/study/dwika/kplchat/login/LoginActivity.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/login/LoginActivity.java
@@ -2,14 +2,23 @@ package com.study.dwika.kplchat.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.study.dwika.kplchat.R;
+
+import butterknife.BindView;
 
 /**
  * Created by Dwika on 28-Nov-17.
  */
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.et_username)
+    EditText mEmailEditText;
+
+    @BindView(R.id.et_password)
+    EditText mPasswordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
