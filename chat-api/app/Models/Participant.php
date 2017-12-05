98d8a6994115fd1d37951b83818f4c8efa7ce33b
muhrifat <muhammad14@mhs.if.its.ac.id>
2017-12-05 19:43:02 +0700

    set up participant model

--- chat-api/app/Models/Participant.php
+++ chat-api/app/Models/Participant.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Participant extends Model
 {
+    use SoftDeletes;
+
     protected $table = "participants";
 
     public $incrementing = true;
     public $timestamps = true;
+
+    protected $dates = [
+        'deleted_at'
+    ];
 
     protected $fillable = [
         'conversation_id',
