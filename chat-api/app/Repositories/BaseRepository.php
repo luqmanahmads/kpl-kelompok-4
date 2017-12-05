18a624717645e2dddf765d2df1ddd31ebf0652e6
muhrifat <muhammad14@mhs.if.its.ac.id>
2017-12-05 19:44:27 +0700

    Create base repository and interface, the repo is general and please see UserRepository as exampe

---
+++ chat-api/app/Repositories/BaseRepository.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mreight
+ * Date: 12/5/2017
+ * Time: 7:28 PM
+ */
+
+namespace App\Repositories;
+
+
+use App\Repositories\Contracts\RepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Mockery\Exception;
+
+abstract class BaseRepository implements RepositoryInterface
+{
+    private $app;
+
+    protected $model;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+        $this->makeModel();
+    }
+
+    abstract function model();
+
+    public function makeModel()
+    {
+        $model = $this->app->make($this->model());
+
+        if (!$model instanceof Model)
+            throw new Exception("error");
+
+        return $this->model = $model;
+    }
+
+    public function all($columns = ['*'])
+    {
+        return $this->model->get($columns);
+    }
+
+    public function paginate($perPage = 15, $columns = ['*'])
+    {
+        return $this->model->paginate($perPage, $columns);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, $id, $attribute="id")
+    {
+        return $this->model->where($attribute, '=', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    public function find($id, $columns = ['*'])
+    {
+        return $this->model->find($id, $columns);
+    }
+
+    public function findBy($field, $value, $columns = ['*'])
+    {
+        return $this->model->where($field, '=', $value)->first($columns);
+    }
+}
---
+++ chat-api/app/Repositories/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mreight
+ * Date: 12/5/2017
+ * Time: 7:32 PM
+ */
+
+namespace App\Repositories;
+
+
+class UserRepository extends BaseRepository
+{
+    function model()
+    {
+        return 'App\Models\User';
+    }
+}
