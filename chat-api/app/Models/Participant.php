a496a1700ea1533f155a9a909de5ecab5636d99c
muhrifat <muhammad14@mhs.if.its.ac.id>
2017-12-05 19:05:33 +0700

    modify participant model

--- chat-api/app/Models/Participant.php
+++ chat-api/app/Models/Participant.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
-    //
+    protected $table = "participants";
+
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'conversation_id',
+        'users_id',
+    ];
+
+    public function conversation()
+    {
+        return $this->belongsTo('App\Models\Conversation');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
