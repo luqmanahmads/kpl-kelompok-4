7ed5fac0355271ea1f4d35e9bbbf23b2ed497108
muhrifat <muhammad14@mhs.if.its.ac.id>
2017-12-05 19:04:21 +0700

    move user class to folder model

--- chat-api/app/Models/User.php
+++ chat-api/app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +16,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'phone'
+    ];
+
+    protected $dates = [
+        'deleted_at'
     ];
 
     /**
@@ -24,6 +29,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'verification_code',
     ];
 }
