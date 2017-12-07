f58d2d31362a36310ec269ea8eaeaf0eee5127ce
muhrifat <muhammad14@mhs.if.its.ac.id>
2017-12-07 23:40:43 +0700

    add endpoint register new user

--- chat-api/routes/api.php
+++ chat-api/routes/api.php
@@ -13,14 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
 
     $api->post('authenticate', 'App\Http\Controllers\Api\v1\AuthenticateController@authenticate');
+    $api->post('logout', 'App\Http\Controllers\Api\v1\AuthenticateController@logout');
+    $api->get('token', 'App\Http\Controllers\Api\v1\AuthenticateController@getToken');
+
+    $api->post('register', 'App\Http\Controllers\Api\v1\AuthenticateController@register');
 
 });
+
+//$api->version('v1', function ($api){
+//
+//    $api->get('authenticated_user', 'App\Http\Controllers\Api\v1\AuthenticateController@authenticatedUser');
+//});
