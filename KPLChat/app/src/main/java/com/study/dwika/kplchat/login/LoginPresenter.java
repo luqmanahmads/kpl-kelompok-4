dea69995bd4e8c9e7df30d67549cf6def80152f2
Dwika Setya <dwikasm@gmail.com>
2017-12-15 16:48:02 +0700

    Login

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/login/LoginPresenter.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/login/LoginPresenter.java
@@ -1,14 +1,12 @@
 package com.study.dwika.kplchat.login;
 
 /**
- * Created by Dwika on 28-Nov-17.
+ * Created by Dwika on 15-Dec-17.
  */
 
-public class LoginPresenter {
-    private LoginView view;
+public class LoginPresenter implements LoginPresenterContract {
+    @Override
+    public void serverLogin(String username, String Password) {
 
-    public LoginPresenter(LoginView view) {
-        this.view = view;
     }
-
 }
--- KPLChat/app/src/main/java/com/study/dwika/kplchat/login/LoginView.java
+++
@@ -1,8 +0,0 @@
-package com.study.dwika.kplchat.login;
-
-/**
- * Created by Dwika on 28-Nov-17.
- */
-
-public interface LoginView {
-}
