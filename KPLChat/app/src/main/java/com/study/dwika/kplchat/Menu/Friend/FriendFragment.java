d7ff657a3534350093ef20dcf29204e09b21b1f3
aditya <ditto.ikhsan@gmail.com>
2017-12-15 23:48:00 +0700

    Add adapter for displaying friend list

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/Menu/Friend/FriendFragment.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/Menu/Friend/FriendFragment.java
@@ -2,6 +2,8 @@ package com.study.dwika.kplchat.menu.Friend;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +19,11 @@ import com.study.dwika.kplchat.data.network.ApiHelper;
 import com.study.dwika.kplchat.data.network.BaseApiHelper;
 import com.study.dwika.kplchat.data.sharedpreference.BaseSharedPreferenceHelper;
 import com.study.dwika.kplchat.data.sharedpreference.SharedPreferenceHelper;
+import com.study.dwika.kplchat.model.UsersResponse;
 import com.study.dwika.kplchat.utils.BaseSchedulerProvider;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by A.I on 27/11/2017.
@@ -31,6 +37,10 @@ public class FriendFragment extends Fragment implements FriendViewContract {
     private BaseDataManager baseDataManager;
     private FriendPresenterContract friendPresenterContract;
     private BaseSchedulerProvider baseSchedulerProvider;
+    private FriendAdapter friendAdapter;
+
+    @BindView(R.id.rvFriend)
+    RecyclerView rvFriend;
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -46,11 +56,24 @@ public class FriendFragment extends Fragment implements FriendViewContract {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_friend, container, false);
+        View view = inflater.inflate(R.layout.fragment_friend, container, false);
+        ButterKnife.bind(this, view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rvFriend.setLayoutManager(layoutManager);
+
+
+        return view;
     }
 
     @Override
     public void getFriend() {
         friendPresenterContract.getFriend(new ApiHeader("Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6Ly9jbGFzc2lmaWVkNS5tZS9jaGF0LWFwaS9hcGkvYXV0aGVudGljYXRlIiwiaWF0IjoxNTEzMzUxNjMzLCJleHAiOjE1MTM0MzgwMzMsIm5iZiI6MTUxMzM1MTYzMywianRpIjoiNzlTN21GVHZCWFVHT1UwUyJ9.jpBWJ_98Z6-_TuoatAIirebb3PHfIp820y1F-CjusqQ", "application/json"));
+    }
+
+    @Override
+    public void displayFriend(UsersResponse usersResponse) {
+        friendAdapter = new FriendAdapter(getContext(), usersResponse.getUsersData());
+        rvFriend.setAdapter(friendAdapter);
+
     }
 }
