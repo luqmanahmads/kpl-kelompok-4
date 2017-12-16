dda51a5b1448aa254b214e746edd987391f59860
aditya <ditto.ikhsan@gmail.com>
2017-12-16 21:41:16 +0700

    Add Database source and conversation adapter

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/Menu/Friend/FriendFragment.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/Menu/Friend/FriendFragment.java
@@ -2,15 +2,12 @@ package com.study.dwika.kplchat.menu.Friend;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-<<<<<<< HEAD
+
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-=======
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
->>>>>>> Add adapter for displaying friend list
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,10 +23,7 @@ import com.study.dwika.kplchat.data.network.ApiHelper;
 import com.study.dwika.kplchat.data.network.BaseApiHelper;
 import com.study.dwika.kplchat.data.sharedpreference.BaseSharedPreferenceHelper;
 import com.study.dwika.kplchat.data.sharedpreference.SharedPreferenceHelper;
-<<<<<<< HEAD
 
-=======
->>>>>>> Add adapter for displaying friend list
 import com.study.dwika.kplchat.model.UsersResponse;
 import com.study.dwika.kplchat.utils.BaseSchedulerProvider;
 
