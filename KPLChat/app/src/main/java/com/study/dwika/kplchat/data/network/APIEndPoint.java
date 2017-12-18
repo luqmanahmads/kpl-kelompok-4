7d581f311ccf3646101d9c91b72277b6cc4dd463
luqmanahmads <luqman_ahmads@ymail.com>
2017-12-18 18:20:54 +0700

    rapihin ui register

--- KPLChat/app/src/main/java/com/study/dwika/kplchat/data/network/APIEndPoint.java
+++ KPLChat/app/src/main/java/com/study/dwika/kplchat/data/network/APIEndPoint.java
@@ -10,7 +10,7 @@ public class ApiEndPoint {
     //public static final String BASE_URL = "http://classified5.me/chat-api/api/";
     //public static final String BASE_URL = "http://10.107.255.221/chat-api/api/";
 //    public static final String BASE_URL = "http://a08f0a7b.ngrok.io/chat-api/api/";
-    public static final String BASE_URL = "http://d67e252c.ngrok.io/chat-api/api/";
+    public static final String BASE_URL = "http://10.107.255.221/chat-api/api/";
 
     // address url selanjutnya ketik di sini
     public static final String LOGIN = BASE_URL + "authenticate";
